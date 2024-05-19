0dbae9a451cdfbab39b51be654600fe2d6797f56
chlqhgus <bocurent@gmail.com>
2024-05-19 19:35:42 +0900

    final version! font changed

--- js/greetings.js
+++ js/greetings.js
@@ -1,32 +1,30 @@
 
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
-
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSummit(event) {
     event.preventDefault();
-    const userName = loginInput.value;
-    localStorage.setItem(USERNAME_KEY,userName);
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY,username);
+
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hi, ${userName}! I wish you have a nice day :)`;
+    greeting.innerText = `Hi, ${username}! I wish you have a nice day 😽🌼`;
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit",onLoginSummit);
-
-if (savedUsername === null){ 
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
+if (savedUsername === null) { //유저네임이 저장되어 있지 않으면 -> 입력 창 보이기
+    loginForm.classList.remove("hidden");
     loginForm.addEventListener("submit",onLoginSummit);
-} else {
-   paintGreetings(); 
-}
-
-function paintGreetings(username) {
-    greeting.innerText = greeting.innerText = `Hi, ${userName}! I wish you have a nice day :)`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-}
+} else { //유저네임이 저장되어 있으면 -> greeting 보이기
+    paintGreetings(loginInput.value);
+} 
